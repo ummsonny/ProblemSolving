0a3e7db01f64242bf7ae896b2efff5c7a60c1d37
ummsonny <zhtkdzjtkd@naver.com>
2022-04-08 23:31:40 +0900

    lg

--- CodingTest/PreviousProblem/DP/prac4.py
+++ CodingTest/PreviousProblem/DP/prac4.py
@@ -7,7 +7,7 @@ dp = [1]*n
 for i in range(1,n):
     for j in range(0,i):
         if array[j]<array[i]:
-            dp[i]=max(dp[i], dp[j]+1)
+            dp[i]=max(dp[j]+1, dp[i])
 
 print(n-max(dp))
 
