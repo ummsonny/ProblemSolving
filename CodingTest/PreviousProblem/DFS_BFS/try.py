a9c28b4c7c93017caf1022eb85940d8cbcd1815e
ummsonny <zhtkdzjtkd@naver.com>
2022-04-13 00:59:53 +0900

    bfs_prac6까지함

--- CodingTest/PreviousProblem/DFS_BFS/try.py
+++ CodingTest/PreviousProblem/DFS_BFS/try.py
@@ -1,39 +1,80 @@
+from cgi import test
+import copy
 n = int(input())
-a_list = list(map(int, input().split()))
-add, sub, mul, div = map(int, input().split())
 
-result_max = int(-1e9)
-result_min = int(1e9)
+graph = []
+teachers = []
 
-def solution(count, now):
-    global result_max, result_min, add, sub, mul, div
+for i in range(n):
+    graph.append(list(input().split()))
+    for j in range(n):
+        if graph[i][j] == 'T':
+            teachers.append((i,j))
+# 조합 문제임
 
-    if count == n:
-        result_max = max(result_max, now)
-        result_min = min(result_min, now)
-
-    if add>0:
-        add-=1
-        solution(count+1, now + a_list[count])
-        add+=1
-
-    if sub>0:
-        sub-=1
-        solution(count+1, now - a_list[count])
-        sub+=1
-
-    if mul>0:
-        mul-=1
-        solution(count+1, now * a_list[count])
-        mul+=1
-
-    if div>0:
-        div-=1
-        solution(count+1, int(now / a_list[count]))
-        div+=1    
-
-solution(1, a_list[0])
-
-print(result_max)
-print(result_min)
+def check():
     
+    for teacher in teachers:
+        #왼쪽 방향 보자!
+        x,y = teacher
+        while y>=0:
+            if graph[x][y]=='S':
+                return True
+            elif graph[x][y]=='O':
+                break
+            y-=1
+            
+        #위쪽 방향 보자!
+        x,y = teacher
+        while x>=0:
+            if graph[x][y]=='S':
+                return True
+            elif graph[x][y]=='O':
+                break
+            x-=1
+
+        #오른쪽 방향 보자!
+        x,y = teacher
+        while y<n:
+            if graph[x][y]=='S':
+                return True
+            elif graph[x][y]=='O':
+                break
+            y+=1
+
+        #아래쪽 방향 보자!
+        x,y = teacher
+        while x<n:
+            if graph[x][y]=='S':
+                return True
+            elif graph[x][y]=='O':
+                break
+            x+=1
+
+    return False # 
+
+
+
+find = True
+
+def dfs(count):
+    global find
+
+    if count == 3:
+        if not check(): #학생 안걸림
+            find = False
+        return
+    
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] == 'X':
+                graph[i][j] = 'O'
+                dfs(count+1)
+                graph[i][j] = 'X'
+
+dfs(0)
+
+if find:
+    print('NO')
+else:
+    print('YES')
